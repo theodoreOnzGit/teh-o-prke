9f99c4d63d90f32ea9480d868b7a3ef56a076682
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-04-12 16:32:06 +0800

    started function for constructing delayed fraction matrix

--- src/zero_power_prke/six_group.rs
+++ src/zero_power_prke/six_group.rs
@@ -1,19 +1,23 @@
 use std::f64::consts::LN_2;
 
 use uom::{si::{f64::*, ratio::ratio, time::second}, ConstZero};
-use approx::*;
-use ndarray::*;
 
-// Decay Constant is essentially the same units as frequency
+/// Decay Constant is essentially the same units as frequency
 pub type DecayConstant = Frequency;
 /// SixGroupPRKE
+#[derive(Debug,Clone,Copy)]
 pub struct SixGroupPRKE {
     /// contains an array for the various half lives 
     /// of the
-    half_life_array: [DecayConstant;6]
+    decay_constant_array: [DecayConstant;6],
+    delayed_fraction_array: [Ratio;6],
+    /// determines the set of delayed group constants based on your choice 
+    /// of fissile isotope
+    pub delayed_group_mode: DelayedGroupMode,
 }
 
 /// different nuclides or fuels have different delayed groups
+#[derive(Debug,Clone,Copy)]
 pub enum DelayedGroupMode {
     /// chooses the U233 group of delayed constants
     U233,
@@ -21,6 +25,52 @@ pub enum DelayedGroupMode {
     U235,
     /// chooses the Pu239 group of delayed constants
     Pu239
+}
+
+impl SixGroupPRKE {
+
+
+    /// constructs the matrix required for 
+    /// solution of implicit six group PRKE
+    pub fn construct_coefficient_matrix(&self,
+        timestep: Time,
+        reactivity: Ratio,
+        neutron_generation_time: Time){
+
+        // preliminaries
+        let lambda_1 = self.decay_constant_array[0];
+        let lambda_2 = self.decay_constant_array[1];
+        let lambda_3 = self.decay_constant_array[2];
+        let lambda_4 = self.decay_constant_array[3];
+        let lambda_5 = self.decay_constant_array[4];
+        let lambda_6 = self.decay_constant_array[5];
+
+        let beta_1 = self.delayed_fraction_array[0];
+        let beta_2 = self.delayed_fraction_array[1];
+        let beta_3 = self.delayed_fraction_array[2];
+        let beta_4 = self.delayed_fraction_array[3];
+        let beta_5 = self.delayed_fraction_array[4];
+        let beta_6 = self.delayed_fraction_array[5];
+
+        let timestep_to_neutron_generation_time_ratio: Ratio = 
+            timestep/neutron_generation_time;
+
+        let total_delayed_fraction: Ratio = 
+            self.delayed_fraction_array.clone().into_iter().sum();
+
+        // bottom row coefficient
+        let bottom_left_coefficient = Ratio::new::<ratio>(1.0) - 
+            timestep_to_neutron_generation_time_ratio*(
+                reactivity - total_delayed_fraction);
+        let delta_t_lambda_1: Ratio = timestep * lambda_1;
+        let delta_t_lambda_2: Ratio = timestep * lambda_2;
+        let delta_t_lambda_3: Ratio = timestep * lambda_3;
+        let delta_t_lambda_4: Ratio = timestep * lambda_4;
+        let delta_t_lambda_5: Ratio = timestep * lambda_5;
+        let delta_t_lambda_6: Ratio = timestep * lambda_6;
+
+        todo!();
+    }
 }
 
 
@@ -145,6 +195,7 @@ pub fn test_u233_total_delayed_frac(){
 #[test]
 pub fn test_u235_total_delayed_frac(){
 
+    use approx::*;
     let total_delayed_fraction_reference = Ratio::new::<ratio>(0.00650);
     
     let u235_delayed_frac_array = new_u235_delayed_neutron_fraction_array();
