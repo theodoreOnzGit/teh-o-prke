85aa26ec7d4b0624ea7019a9fa46d0b31ed60993
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-04-12 16:19:29 +0800

    added delayed fractions and asserted that these are equal

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+
 #[warn(missing_docs)]
 /// contains structs for the zero power point reactor kinetics equations
 pub mod zero_power_prke;
--- src/zero_power_prke/six_group.rs
+++ src/zero_power_prke/six_group.rs
@@ -1,15 +1,16 @@
 use std::f64::consts::LN_2;
 
-use uom::{si::{f64::*, time::second}, ConstZero};
+use uom::{si::{f64::*, ratio::ratio, time::second}, ConstZero};
+use approx::*;
 use ndarray::*;
 
-// Decay rate is essentially the same units as frequency
-pub type DecayRate = Frequency;
+// Decay Constant is essentially the same units as frequency
+pub type DecayConstant = Frequency;
 /// SixGroupPRKE
 pub struct SixGroupPRKE {
     /// contains an array for the various half lives 
     /// of the
-    half_life_array: [DecayRate;6]
+    half_life_array: [DecayConstant;6]
 }
 
 /// different nuclides or fuels have different delayed groups
@@ -23,11 +24,12 @@ pub enum DelayedGroupMode {
 }
 
 
-pub fn new_decay_constant_array() -> [DecayRate;6] {
+/// produces a new decay constant array
+pub fn new_decay_constant_array() -> [DecayConstant;6] {
 
     // from table 5.1 (not sure where from, duderstadt?)
     // need to double check
-    let mut decay_constant_array: [DecayRate;6] = 
+    let mut decay_constant_array: [DecayConstant;6] = 
         [
         Frequency::ZERO,
         Frequency::ZERO,
@@ -48,6 +50,8 @@ pub fn new_decay_constant_array() -> [DecayRate;6] {
         ];
 
     // crude, but gets the job done
+    //
+    // lambda (decay constant) = ln(2)/(half life)
     decay_constant_array[0] = LN_2/half_life_array[0];
     decay_constant_array[1] = LN_2/half_life_array[1];
     decay_constant_array[2] = LN_2/half_life_array[2];
@@ -58,5 +62,99 @@ pub fn new_decay_constant_array() -> [DecayRate;6] {
     // done!
     decay_constant_array
         
+
+}
+
+/// produces a new delayed fraction for u233 
+pub fn new_u233_delayed_neutron_fraction_array() -> [Ratio;6] {
+
+    let delayed_neutron_array: [Ratio;6] = 
+        [
+        Ratio::new::<ratio>(0.00023),
+        Ratio::new::<ratio>(0.00078),
+        Ratio::new::<ratio>(0.00064),
+        Ratio::new::<ratio>(0.00074),
+        Ratio::new::<ratio>(0.00014),
+        Ratio::new::<ratio>(0.00008),
+        ];
+
+    delayed_neutron_array
+}
+
+/// produces a new delayed fraction for u235 
+pub fn new_u235_delayed_neutron_fraction_array() -> [Ratio;6] {
+
+    let delayed_neutron_array: [Ratio;6] = 
+        [
+        Ratio::new::<ratio>(0.00021),
+        Ratio::new::<ratio>(0.00142),
+        Ratio::new::<ratio>(0.00128),
+        Ratio::new::<ratio>(0.00257),
+        Ratio::new::<ratio>(0.00075),
+        Ratio::new::<ratio>(0.00027),
+        ];
+
+    delayed_neutron_array
+}
+
+
+/// produces a new delayed fraction for pu239 
+pub fn new_pu239_delayed_neutron_fraction_array() -> [Ratio;6] {
+
+    let delayed_neutron_array: [Ratio;6] = 
+        [
+        Ratio::new::<ratio>(0.00007),
+        Ratio::new::<ratio>(0.00063),
+        Ratio::new::<ratio>(0.00044),
+        Ratio::new::<ratio>(0.00069),
+        Ratio::new::<ratio>(0.00018),
+        Ratio::new::<ratio>(0.00009),
+        ];
+
+    delayed_neutron_array
+}
+
+#[test]
+pub fn test_pu239_total_delayed_frac(){
+
+    let total_delayed_fraction_reference = Ratio::new::<ratio>(0.00210);
+    
+    let pu239_delayed_frac_array = new_pu239_delayed_neutron_fraction_array();
+
+    let test_delayed_frac: Ratio = pu239_delayed_frac_array.into_iter().sum();
+
+    assert_eq!(test_delayed_frac,total_delayed_fraction_reference)
+
+
+}
+
+#[test]
+pub fn test_u233_total_delayed_frac(){
+
+    let total_delayed_fraction_reference = Ratio::new::<ratio>(0.00261);
+    
+    let u233_delayed_frac_array = new_u233_delayed_neutron_fraction_array();
+
+    let test_delayed_frac: Ratio = u233_delayed_frac_array.into_iter().sum();
+
+    assert_eq!(test_delayed_frac,total_delayed_fraction_reference)
+
+
+}
+
+#[test]
+pub fn test_u235_total_delayed_frac(){
+
+    let total_delayed_fraction_reference = Ratio::new::<ratio>(0.00650);
+    
+    let u235_delayed_frac_array = new_u235_delayed_neutron_fraction_array();
+
+    let test_delayed_frac: Ratio = u235_delayed_frac_array.into_iter().sum();
+
+    assert_abs_diff_eq!(
+        test_delayed_frac.get::<ratio>(), 
+        total_delayed_fraction_reference.get::<ratio>(), 
+        epsilon = f64::EPSILON);
+
 
 }
