3dbcebe94365ec6e43446c4add20db2d2e0b09b9
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-04-22 15:00:32 +0800

    added barebones structure for fhr simulator app

--- examples/fhr_sim_v1/main.rs
+++ examples/fhr_sim_v1/main.rs
@@ -1,4 +1,4 @@
-use std::thread;
+use std::{thread, time::Duration};
 
 /// this represents the first iteration 
 /// of the fhr simulator
@@ -9,10 +9,11 @@ use std::thread;
 #[cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 fn main(){
 
+    fhr_simulator_v1().unwrap();
 
 
 }
-pub fn ciet_simulator_v1() -> eframe::Result<()> {
+pub fn fhr_simulator_v1() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
@@ -20,14 +21,14 @@ pub fn ciet_simulator_v1() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "CIET Simulator V1 Powered by TUAS",
+        "FHR Simulator V1 Powered by TUAS, tampines-steam-tables and teh-o-prke",
         native_options,
         Box::new(|cc| {
             // image support,
             // from 
             // https://github.com/emilk/egui/tree/master/examples/images
             egui_extras::install_image_loaders(&cc.egui_ctx);
-            Ok(Box::new(CIETApp::new(cc)))
+            Ok(Box::new(FHRSimulatorApp::new(cc)))
 
     }
 
@@ -36,10 +37,10 @@ pub fn ciet_simulator_v1() -> eframe::Result<()> {
 }
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct CIETApp {
+pub struct FHRSimulatorApp {
 
 }
-impl CIETApp {
+impl FHRSimulatorApp {
     /// Called once before the first frame.
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -51,7 +52,7 @@ impl CIETApp {
         //    return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         //}
 
-        let new_ciet_app: CIETApp = Default::default();
+        let new_ciet_app: FHRSimulatorApp = Default::default();
 
         // now spawn a thread moving in the pointer 
         //
@@ -69,7 +70,7 @@ impl CIETApp {
 
     
 }
-impl Default for CIETApp {
+impl Default for FHRSimulatorApp {
     fn default() -> Self {
 
 
@@ -78,4 +79,65 @@ impl Default for CIETApp {
 
         }
     }
+}
+impl eframe::App for FHRSimulatorApp {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+        });
+
+        egui::SidePanel::right("Supplementary Info").show(ctx, |ui|{
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("CIET Educational Simulator v1");
+            ui.separator();
+            egui::menu::bar(ui, |ui| {
+
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
+
+        egui::TopBottomPanel::bottom("github").show(ctx, |ui|{
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                powered_by_egui_and_eframe(ui);
+                egui::warn_if_debug_build(ui);
+            });
+
+        });
+
+        
+
+
+        ctx.request_repaint_after(Duration::from_millis(50));
+
+        // adding the return here because there are too many closing 
+        // parantheses
+        // just demarcates the end
+        return ();
+    }
+}
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
 }
