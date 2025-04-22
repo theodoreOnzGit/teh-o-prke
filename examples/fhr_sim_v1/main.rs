b5631fbbb3a1b6d3dfd10b3f36643ead13b5a67f
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-04-22 14:43:39 +0800

    moved the examples from src to examples

---
+++ examples/fhr_sim_v1/main.rs
@@ -0,0 +1,11 @@
+/// this represents the first iteration 
+/// of the fhr simulator
+///
+/// basically one can do a FHR loop 
+/// with a permenantly steady state steam cycle
+/// the latter uses the tampines-steam-tables
+fn main(){
+
+
+
+}
