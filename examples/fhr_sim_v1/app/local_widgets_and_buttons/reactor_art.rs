abe0907575095112068e1978daedec042f734d87
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2025-04-24 10:50:01 +0800

    added fhr reactor shape part i

--- examples/fhr_sim_v1/app/local_widgets_and_buttons/reactor_art.rs
+++ examples/fhr_sim_v1/app/local_widgets_and_buttons/reactor_art.rs
@@ -1,6 +1,8 @@
 use std::f32::consts::TAU;
 
-use egui::{vec2, Color32, Pos2, Rect, Sense, Stroke, Ui, Vec2};
+use egui::{epaint::PathShape, vec2, Color32, Pos2, Rect, Sense, Stroke, Ui, Vec2};
+
+use super::hot_to_cold_colour_mark_1;
 
 // first thing, reactor
 //
@@ -22,8 +24,8 @@ pub fn fhr_reactor_vessel(ui: &mut Ui,
     let right_most_side = user_rect.right();
     let bottom_most_side = user_rect.bottom();
 
-    let max_height = (top_most_side - bottom_most_side).abs();
-    let max_width = (left_most_side - right_most_side).abs();
+    let max_height_y = (top_most_side - bottom_most_side).abs();
+    let max_width_x = (left_most_side - right_most_side).abs();
 
     let ui_rectangle: Rect = ui.min_rect();
     let breadth = ui_rectangle.right();
@@ -55,19 +57,19 @@ pub fn fhr_reactor_vessel(ui: &mut Ui,
     let color = Color32::from_gray(128);
     let stroke = Stroke::new(1.0, color);
 
-    let reactor_half_length = max_height * 0.5 * 0.8;
-    let reactor_half_width = max_width * 0.5 * 0.8;
+    let reactor_half_length_y = max_height_y * 0.5 * 0.8;
+    let reactor_half_width_x = max_width_x * 0.5 * 0.8;
     //painter.circle_stroke(c, r, stroke);
     
     // now let's paint the reactor first
     painter.line_segment(
-        [c + vec2(reactor_half_width, reactor_half_length), 
-        c + vec2(reactor_half_width, -reactor_half_length)], 
+        [c + vec2(reactor_half_width_x, reactor_half_length_y), 
+        c + vec2(reactor_half_width_x, -reactor_half_length_y)], 
         stroke
     );
     painter.line_segment(
-        [c + vec2(-reactor_half_width, reactor_half_length), 
-        c + vec2(-reactor_half_width, -reactor_half_length)], 
+        [c + vec2(-reactor_half_width_x, reactor_half_length_y), 
+        c + vec2(-reactor_half_width_x, -reactor_half_length_y)], 
         stroke
     );
     //painter.line_segment([c, c + r * Vec2::angled(TAU * 1.0 / 8.0)], stroke);
@@ -85,7 +87,56 @@ pub fn fhr_reactor_vessel(ui: &mut Ui,
     // to the painter:
     // 
     // https://docs.rs/egui/latest/egui/struct.Painter.html
+    //
+    // so to get the shape, i have a complex polygon
+    // The points are
 
+    
+
+    // bottom inlet
+    let fhr_coolant_inlet_bottom_left = c + vec2(-0.10*reactor_half_width_x, reactor_half_length_y* 0.9);
+    let fhr_coolant_inlet_bottom_right = c + vec2(0.10*reactor_half_width_x, reactor_half_length_y* 0.9);
+
+    // core part
+    let fhr_core_inlet_bottom_left = c + vec2(-0.10*reactor_half_width_x, reactor_half_length_y* 0.75);
+    let fhr_core_inlet_bottom_right = c + vec2(0.10*reactor_half_width_x, reactor_half_length_y* 0.75);
+    let fhr_core_fat_bottom_left = c + vec2(-0.50*reactor_half_width_x, reactor_half_length_y* 0.55);
+    let fhr_core_fat_bottom_right = c + vec2(0.50*reactor_half_width_x, reactor_half_length_y* 0.55);
+
+    let fhr_core_fat_top_left = c + vec2(-0.50*reactor_half_width_x, -reactor_half_length_y* 0.55);
+    let fhr_core_fat_top_right = c + vec2(0.50*reactor_half_width_x, -reactor_half_length_y* 0.55);
+    let fhr_core_outlet_top_left = c + vec2(-0.10*reactor_half_width_x, -reactor_half_length_y* 0.75);
+    let fhr_core_outlet_top_right = c + vec2(0.10*reactor_half_width_x, -reactor_half_length_y* 0.75);
+
+    // top outlet
+    let fhr_coolant_outlet_top_left = c + vec2(-0.10*reactor_half_width_x, -reactor_half_length_y* 0.9);
+    let fhr_coolant_outlet_top_right = c + vec2(0.10*reactor_half_width_x, -reactor_half_length_y* 0.9);
+
+    // colour fill
+    let hotness: f32 = 0.1;
+    let fill = hot_to_cold_colour_mark_1(hotness);
+
+    // draw clockwise
+    let points = vec![
+        //fhr_coolant_inlet_bottom_left,
+        fhr_core_inlet_bottom_left,
+        fhr_core_fat_bottom_left,
+        //fhr_core_fat_top_left,
+        //fhr_core_outlet_top_left,
+        //fhr_coolant_outlet_top_left,
+        //fhr_coolant_outlet_top_right,
+        //fhr_core_outlet_top_right,
+        //fhr_core_fat_top_right,
+        fhr_core_fat_bottom_right,
+        fhr_core_inlet_bottom_right,
+        //fhr_coolant_inlet_bottom_right,
+    ];
+    
+    // fhr coolant 
+    let fhr_core_bottom_coolant_shape = 
+        PathShape::convex_polygon(points, fill, stroke);
+
+    painter.add(fhr_core_bottom_coolant_shape);
 
 }
 
