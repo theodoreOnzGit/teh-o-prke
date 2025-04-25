4bb6040deafa92e625a5ed804537ee289ccc8f61
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-04-25 15:17:23 +0800

    tidying up the prke thingys

--- src/zero_power_prke/six_group.rs
+++ src/zero_power_prke/six_group.rs
@@ -16,9 +16,11 @@ pub type DecayConstant = Frequency;
 #[derive(Debug,Clone,Copy)]
 pub struct SixGroupPRKE {
     /// contains an array for the various half lives 
-    /// of the
-    decay_constant_array: [DecayConstant;6],
-    delayed_fraction_array: [Ratio;6],
+    /// of the delayed precursors
+    pub decay_constant_array: [DecayConstant;6],
+    /// contains delayed fraction arrays for the delayed precursors 
+    /// this is different for u235, u233 and Pu239
+    pub delayed_fraction_array: [Ratio;6],
     /// determines the set of delayed group constants based on your choice 
     /// of fissile isotope
     pub delayed_group_mode: DelayedGroupMode,
@@ -71,107 +73,6 @@ impl DelayedGroupMode {
     }
 }
 
-#[test]
-pub fn prke_test_zero_reactivity(){
-    // let's make a new six group PRKE struct,
-    // with zero reactivity and zero new source 
-    // we should get a constant neutron pop 
-
-    use uom::si::time::{millisecond, nanosecond};
-    use approx::assert_abs_diff_eq;
-    let delayed_group_mode = DelayedGroupMode::U235;
-
-
-    let precursor_and_neutron_pop_and_source_array: [VolumetricNumberDensity;7] = 
-        [
-        VolumetricNumberDensity::new::<per_cubic_meter>(1.0),
-        VolumetricNumberDensity::ZERO,
-        VolumetricNumberDensity::ZERO,
-        VolumetricNumberDensity::ZERO,
-        VolumetricNumberDensity::ZERO,
-        VolumetricNumberDensity::ZERO,
-        VolumetricNumberDensity::ZERO,
-        ];
-    let decay_constant_array = delayed_group_mode.get_decay_constant_array();
-    let delayed_fraction_array = delayed_group_mode.get_delayed_fraction_array();
-
-    let mut prke_test = SixGroupPRKE {
-        decay_constant_array,
-        delayed_fraction_array,
-        delayed_group_mode,
-        precursor_and_neutron_pop_and_source_array,
-    };
-
-    let timestep = Time::new::<millisecond>(50.0);
-    let neutron_generation_time = Time::new::<nanosecond>(10.0);
-    let zero_reactivity = Ratio::ZERO;
-    let background_source_rate = VolumetricNumberRate::ZERO;
-
-    // now before running a timestep, we should get a neutron pop of 1 per m3
-    let initial_neutron_pop_float = prke_test.get_current_neutron_population().
-        get::<per_cubic_meter>();
-
-    assert_eq!(initial_neutron_pop_float,1.0);
-
-    // now let's run a timestep with zero reactivity
-
-    let number_of_timesteps = 10000;
-
-    for _ in 0..number_of_timesteps {
-
-        prke_test.solve_next_timestep_precursor_concentration_and_neutron_pop_vector(
-            timestep, 
-            zero_reactivity, 
-            neutron_generation_time, 
-            background_source_rate).unwrap();
-    }
-
-
-    // makes sense that we dont add to one because of the delayed precursors 
-    //
-    // however, the sum of all precursor concentrations and neutron 
-    // populations should be 1.0
-
-    let precursor_sum_with_neutron_pop_array = 
-        prke_test.solve_next_timestep_precursor_concentration_and_neutron_pop_vector(
-            timestep, 
-            zero_reactivity, 
-            neutron_generation_time, 
-            background_source_rate).unwrap();
-
-    let precursor_and_neutron_pop_sum: VolumetricNumberDensity
-        = precursor_sum_with_neutron_pop_array.into_iter().sum();
-
-
-    assert_abs_diff_eq!(
-        precursor_and_neutron_pop_sum.get::<per_cubic_meter>(),
-        1.0,
-        epsilon = 1e-9);
-
-    // now the neutron population after 50 ms * 10000 timesteps is:
-    assert_abs_diff_eq!(
-        prke_test.get_current_neutron_population().value,
-        1.17917639e-7,
-        epsilon = 1e-11);
-
-    // if critical, it should not change after next 500 s
-
-    for _ in 0..number_of_timesteps {
-
-        prke_test.solve_next_timestep_precursor_concentration_and_neutron_pop_vector(
-            timestep, 
-            zero_reactivity, 
-            neutron_generation_time, 
-            background_source_rate).unwrap();
-    }
-
-    // now the neutron population after another 50 ms * 10000 timesteps is:
-    // or else 500s should be the same
-    assert_abs_diff_eq!(
-        prke_test.get_current_neutron_population().value,
-        1.17917639e-7,
-        epsilon = 1e-11);
-}
 
 /// default is to use u235 decay constants and delayed fraction, with 
 /// starting neutron population of 1 per m3
