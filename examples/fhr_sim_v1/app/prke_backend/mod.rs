1a2e1bd1e839e38ae34a8b705455fdb8896976ca
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2025-04-27 12:15:48 +0800

    added some code to calculate decay heat, not tested yet

--- examples/fhr_sim_v1/app/prke_backend/mod.rs
+++ examples/fhr_sim_v1/app/prke_backend/mod.rs
@@ -1,3 +1,4 @@
+use std::f64::consts::LN_2;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
@@ -146,4 +147,114 @@ impl FHRSimulatorApp {
 
     
     
+}
+
+/// this struct helps to manage decay heat calculations
+///
+/// similar to how delayed neutron precursors are handled,
+/// I have some groups for decay heat precursors. 
+/// These are loosely coupled (semi-implicit)
+#[derive(Clone, Debug, Copy)]
+pub struct FHRDecayHeat {
+    decay_heat_precursor1: Power,
+    pub decay_heat_precursor1_half_life: Time,
+    decay_heat_precursor2: Power,
+    pub decay_heat_precursor2_half_life: Time,
+    decay_heat_precursor3: Power,
+    pub decay_heat_precursor3_half_life: Time,
+}
+
+impl FHRDecayHeat {
+    pub fn add_decay_heat_precursor1(&mut self, 
+        decay_precursor_power: Power){
+
+        self.decay_heat_precursor1 += decay_precursor_power;
+    }
+    pub fn add_decay_heat_precursor2(&mut self, 
+        decay_precursor_power: Power){
+
+        self.decay_heat_precursor2 += decay_precursor_power;
+    }
+    pub fn add_decay_heat_precursor3(&mut self, 
+        decay_precursor_power: Power){
+
+        self.decay_heat_precursor3 += decay_precursor_power;
+    }
+
+
+    /// basically 
+    ///
+    /// (P_decay^(t + Delta t) - P_decay^t)/ Delta t = - lambda_i P_decay^(t + Delta t)
+    pub fn calc_decay_heat_power_1(&mut self,
+        timestep: Time) -> Power {
+
+        let decay_constant: Frequency = 
+            LN_2/self.decay_heat_precursor1_half_life;
+        // (P_decay^(t + Delta t) - P_decay^t)/ Delta t = - lambda_i P_decay^(t + Delta t)
+        //
+        // P_decay^(t + Delta t) (1 + lambda_i * Delta t) = P_decay^t
+        //
+
+        let p_decay_t = self.decay_heat_precursor1;
+        let coeff = Ratio::new::<ratio>(1.0) + decay_constant * timestep;
+
+        let p_decay_t_plus_delta_t = p_decay_t / coeff;
+
+        self.decay_heat_precursor1 = p_decay_t_plus_delta_t;
+
+        return p_decay_t_plus_delta_t;
+
+        
+
+    }
+    /// basically 
+    ///
+    /// (P_decay^(t + Delta t) - P_decay^t)/ Delta t = - lambda_i P_decay^(t + Delta t)
+    pub fn calc_decay_heat_power_2(&mut self,
+        timestep: Time) -> Power {
+
+        let decay_constant: Frequency = 
+            LN_2/self.decay_heat_precursor2_half_life;
+        // (P_decay^(t + Delta t) - P_decay^t)/ Delta t = - lambda_i P_decay^(t + Delta t)
+        //
+        // P_decay^(t + Delta t) (1 + lambda_i * Delta t) = P_decay^t
+        //
+
+        let p_decay_t = self.decay_heat_precursor2;
+        let coeff = Ratio::new::<ratio>(1.0) + decay_constant * timestep;
+
+        let p_decay_t_plus_delta_t = p_decay_t / coeff;
+
+        self.decay_heat_precursor2 = p_decay_t_plus_delta_t;
+
+        return p_decay_t_plus_delta_t;
+
+        
+
+    }
+    /// basically 
+    ///
+    /// (P_decay^(t + Delta t) - P_decay^t)/ Delta t = - lambda_i P_decay^(t + Delta t)
+    pub fn calc_decay_heat_power_3(&mut self,
+        timestep: Time) -> Power {
+
+        let decay_constant: Frequency = 
+            LN_2/self.decay_heat_precursor3_half_life;
+        // (P_decay^(t + Delta t) - P_decay^t)/ Delta t = - lambda_i P_decay^(t + Delta t)
+        //
+        // P_decay^(t + Delta t) (1 + lambda_i * Delta t) = P_decay^t
+        //
+
+        let p_decay_t = self.decay_heat_precursor3;
+        let coeff = Ratio::new::<ratio>(1.0) + decay_constant * timestep;
+
+        let p_decay_t_plus_delta_t = p_decay_t / coeff;
+
+        self.decay_heat_precursor3 = p_decay_t_plus_delta_t;
+
+        return p_decay_t_plus_delta_t;
+
+        
+
+    }
 }
