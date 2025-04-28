4abc78f5510aa4d4ac781fb2cb585acc7a8a9491
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2025-04-28 08:12:55 +0800

    added decay heat plus thermal hydraulics module

--- examples/fhr_sim_v1/app/prke_backend/mod.rs
+++ examples/fhr_sim_v1/app/prke_backend/mod.rs
@@ -4,14 +4,17 @@ use std::thread;
 use std::time::Duration;
 
 use teh_o_prke::{feedback_mechanisms::SixFactorFormulaFeedback, zero_power_prke::six_group::SixGroupPRKE};
+use uom::si::available_energy::joule_per_kilogram;
 use uom::si::energy::megaelectronvolt;
 use uom::si::linear_number_density::per_meter;
-use uom::si::time::second;
+use uom::si::specific_heat_capacity::joule_per_kilogram_kelvin;
+use uom::si::time::{day, hour, second};
 use uom::si::velocity::meter_per_second;
 use uom::si::volume::cubic_meter;
 use uom::si::volumetric_number_rate::per_cubic_meter_second;
 use uom::si::{f64::*, ratio::ratio};
-use uom::si::thermodynamic_temperature::degree_celsius;
+use uom::si::thermodynamic_temperature::{degree_celsius, kelvin};
+use uom::ConstZero;
 
 use crate::{FHRSimulatorApp, FHRState};
 
@@ -52,6 +55,10 @@ impl FHRSimulatorApp {
         let reactor_volume = Volume::new::<cubic_meter>(0.5);
         let macroscopic_fission_xs = LinearNumberDensity::new::<per_meter>(1.0);
         let mut fhr_state_clone = fhr_state.lock().unwrap().clone();
+
+        // then decay heat struct 
+        let mut fhr_decay_heat = FHRDecayHeat::default();
+
         loop {
             Self::calculate_prke_for_one_timestep(&mut fhr_state_clone,
                 &mut keff_six_factor,
@@ -59,6 +66,7 @@ impl FHRSimulatorApp {
                 timestep,
                 reactor_volume,
                 macroscopic_fission_xs,
+                &mut fhr_decay_heat,
             );
             let dur = Duration::from_millis(40);
             thread::sleep(dur);
@@ -74,6 +82,7 @@ impl FHRSimulatorApp {
         timestep: Time,
         reactor_volume: Volume,
         macroscopic_fission_xs: LinearNumberDensity,
+        fhr_decay_heat: &mut FHRDecayHeat,
         ){
 
         // within each timestep, I need to obtain feedback
@@ -138,9 +147,25 @@ impl FHRSimulatorApp {
             Energy::new::<megaelectronvolt>(200.0);
 
         // immediate power from fission
-        let fission_power: Power = 
+        let mut fission_power: Power = 
             power_per_fission * fission_rate;
 
+        // add to decay heat precursors 
+        fhr_decay_heat.add_decay_heat_precursor1(fission_power * 0.04);
+        fhr_decay_heat.add_decay_heat_precursor2(fission_power * 0.04);
+        fhr_decay_heat.add_decay_heat_precursor3(fission_power * 0.02);
+
+
+        // adjust fission power for decay heat 
+        // fission power less decay heat = 1.0 - 0.04 - 0.04 - 0.02 = 0.9
+        fission_power *= 0.9;
+        fission_power += fhr_decay_heat.calc_decay_heat_power_1(timestep);
+        fission_power += fhr_decay_heat.calc_decay_heat_power_2(timestep);
+        fission_power += fhr_decay_heat.calc_decay_heat_power_3(timestep);
+
+        // with the correct fission power now, we can 
+        // calc temperature
+        //
 
 
     }
@@ -148,6 +173,7 @@ impl FHRSimulatorApp {
     
     
 }
+
 
 /// this struct helps to manage decay heat calculations
 ///
@@ -256,5 +282,124 @@ impl FHRDecayHeat {
 
         
 
+    }
+
+}
+
+/// default is half lives of 
+///
+/// 0.2 hrs
+/// 8 hrs 
+/// 30 days 
+///
+/// then fission power ratio is:
+/// 30 days -> 2%
+/// 8 hrs -> 4% 
+/// 0.2 hrs -> 4%
+impl Default for FHRDecayHeat {
+    fn default() -> Self {
+
+        Self { 
+            decay_heat_precursor1: Power::ZERO, 
+            decay_heat_precursor1_half_life: Time::new::<hour>(0.2), 
+            decay_heat_precursor2: Power::ZERO, 
+            decay_heat_precursor2_half_life: Time::new::<hour>(8.0), 
+            decay_heat_precursor3: Power::ZERO, 
+            decay_heat_precursor3_half_life: Time::new::<day>(30.0),
+        }
+    }
+}
+
+
+/// this struct helps to manage pebble bed thermal hydraulics 
+/// calculations
+///
+/// This is important for fast feedback of fuel temperature 
+///
+/// basically it keeps track of the thermal inertia of the system
+/// among other things
+///
+#[derive(Clone, Debug, Copy)]
+pub struct PebbleBedThermalHydraulics {
+    /// this is the current specific enthalpy of the pebble bed
+    pub current_fuel_specific_enthalpy: AvailableEnergy,
+}
+
+impl PebbleBedThermalHydraulics {
+
+    /// creates a new instance of PebbleBedThermalHydraulics
+    pub fn new() -> Self {
+        // let's start pebble bed at 500 degc 
+        let start_temp = ThermodynamicTemperature::new::<degree_celsius>(500.0);
+        let start_enthalpy = Self::get_enthalpy_from_temperature_uo2(start_temp);
+
+        return Self { current_fuel_specific_enthalpy: start_enthalpy };
+    }
+
+    /// Carbajo, J. J. (2001). Thermophysical properties of MOX and UO2 
+    /// fuels including the effects of irradiation (No. ORNL/TM-2000/351). 
+    /// Oak Ridge National Lab.(ORNL), Oak Ridge, TN (United States).
+    ///
+    /// enthalpy at 298K is deemd to be zero J/kg
+    pub fn get_enthalpy_from_temperature_uo2(
+        fuel_temp: ThermodynamicTemperature
+    ) -> AvailableEnergy {
+
+        // see page 18
+        // table 4.2
+        // for uranium oxide
+
+        let c1 = SpecificHeatCapacity::new::<joule_per_kilogram_kelvin>(
+            302.27
+        );
+        let c2 = SpecificHeatCapacity::new::<joule_per_kilogram_kelvin>(
+            8.463e-3
+        ) / TemperatureInterval::new::<uom::si::temperature_interval::kelvin>(1.0);
+        let c3 = AvailableEnergy::new::<joule_per_kilogram>(
+            8.741e7
+        );
+
+        // einstein temp
+        let theta = ThermodynamicTemperature::new::<kelvin>(
+            548.68
+        );
+
+        // electron activation energy divide by boltzmann constant 
+        let e_a = ThermodynamicTemperature::new::<kelvin>(
+            18_531.7
+        );
+
+        // some terms for calc 
+
+        let ref_temp_298 = ThermodynamicTemperature::new::<kelvin>(298.0);
+        let theta_by_t: Ratio = theta/fuel_temp;
+        let theta_by_298: Ratio = theta/ref_temp_298;
+
+        let e_a_by_t: Ratio = e_a/fuel_temp;
+
+        // calc term by term
+        let term1: AvailableEnergy = c1 * theta * (
+            (theta_by_t.get::<ratio>().exp() - 1.0).recip()
+            -(theta_by_298.get::<ratio>().exp() - 1.0).recip()
+        );
+
+        let term2: AvailableEnergy = 
+            c2 * (fuel_temp * fuel_temp - ref_temp_298 * ref_temp_298);
+
+        let term3: AvailableEnergy = c3 * (-e_a_by_t).exp();
+
+        // enthalpy increment over 298K 
+        let enthalpy_increment_over_298_kelvin = 
+            term1 + term2 + term3;
+
+        return enthalpy_increment_over_298_kelvin;
+
+
+
+
+    }
+
+    pub fn get_current_temeperature(&self) -> ThermodynamicTemperature {
+        todo!()
     }
 }
