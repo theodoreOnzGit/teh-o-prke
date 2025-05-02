6895877487c0ba3c57cb04ee1c95dcbb00ee933c
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-05-02 17:36:39 +0800

    added some equations for iodine 135

--- src/feedback_mechanisms/fission_product_poisons/mod.rs
+++ src/feedback_mechanisms/fission_product_poisons/mod.rs
@@ -1,4 +1,8 @@
-use uom::{si::{f64::*, ratio::ratio}, ConstZero};
+use uom::si::frequency::hertz;
+use uom::ConstZero;
+use uom::si::{f64::*, ratio::ratio};
+
+use crate::zero_power_prke::six_group::FissioningNuclideType;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Xenon135Poisoning {
@@ -52,6 +56,60 @@ impl Xenon135Poisoning {
     pub fn fp_yield_xe_135_from_pu239_thermal_fission() -> Ratio {
         Ratio::new::<ratio>(0.00105)
     }
+
+    /// table 7.6
+    /// Lamarsh, J. R. (1975). Introduction to nuclear engineering.
+    /// returns decay constant for a particular nuclide
+    #[inline]
+    pub fn iodine_135_decay_const() -> Frequency {
+        Frequency::new::<hertz>(2.87e-5)
+    }
+
+
+    /// table 7.6
+    /// Lamarsh, J. R. (1975). Introduction to nuclear engineering.
+    /// returns decay constant for a particular nuclide
+    #[inline]
+    pub fn xe_135_decay_const() -> Frequency {
+        Frequency::new::<hertz>(2.09e-5)
+    }
+
+
+    /// (dI/dt) = gamma_I * fission rate -  lambda_I * I
+    ///
+    /// (I ^(t + delta t) - I^t)/ (delta t) = gamma_I * fission_rate - lambda_I I^(t + delta t)
+    /// (I ^(t + delta t) - I^t) = delta t * gamma_I * fission_rate - delta t * lambda_I I^(t + delta t)
+    /// (I ^(t + delta t) + delta t * lambda_I I^(t + delta t)) = I^t + delta t * gamma_I * fission_rate 
+    /// I ^(t + delta t)(1 + delta t * lambda_I ) = I^t + delta t * gamma_I * fission_rate 
+    #[inline]
+    pub fn calc_iodine_135_and_return_conc(&mut self,
+        timestep: Time,
+        fission_rate: VolumetricNumberRate,
+        fissioning_nuclide: FissioningNuclideType) -> VolumetricNumberDensity {
+
+        let gamma_i = match fissioning_nuclide {
+            FissioningNuclideType::U233 => Self::fp_yield_iodine_135_from_u233_thermal_fission(),
+            FissioningNuclideType::U235 => Self::fp_yield_iodine_135_from_u235_thermal_fission(),
+            FissioningNuclideType::Pu239 => Self::fp_yield_iodine_135_from_pu239_thermal_fission(),
+        };
+
+        let current_iodine_conc = self.iodine_135_concentration;
+        let additional_iodine_conc: VolumetricNumberDensity 
+            = (timestep * fission_rate * gamma_i).into();
+
+        let rhs = current_iodine_conc + additional_iodine_conc;
+
+        let new_iodine_conc: VolumetricNumberDensity = 
+            (
+                rhs / (Ratio::new::<ratio>(1.0) + timestep * Self::iodine_135_decay_const())
+            ).into();
+
+        self.iodine_135_concentration = new_iodine_conc;
+
+        return new_iodine_conc;
+
+    }
+
 }
 
 impl Default for Xenon135Poisoning {
