96d05b3820ab3405aa0b477d61168b14f834d069
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-05-02 17:22:46 +0800

    added fission product yields for i135 and xe135 for iodine, xe

--- src/feedback_mechanisms/fission_product_poisons/mod.rs
+++ src/feedback_mechanisms/fission_product_poisons/mod.rs
@@ -1,4 +1,4 @@
-use uom::{si::f64::*, ConstZero};
+use uom::{si::{f64::*, ratio::ratio}, ConstZero};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Xenon135Poisoning {
@@ -8,6 +8,50 @@ pub struct Xenon135Poisoning {
 
 impl Xenon135Poisoning {
 
+    /// table 7.5 
+    /// returns number of atoms per thermal fission of a nuclide
+    /// Lamarsh, J. R. (1975). Introduction to nuclear engineering.
+    #[inline]
+    pub fn fp_yield_iodine_135_from_u235_thermal_fission() -> Ratio {
+        Ratio::new::<ratio>(0.0639)
+    }
+    /// returns number of atoms per thermal fission of a nuclide
+    /// Lamarsh, J. R. (1975). Introduction to nuclear engineering.
+    #[inline]
+    pub fn fp_yield_xe_135_from_u235_thermal_fission() -> Ratio {
+        Ratio::new::<ratio>(0.00237)
+    }
+
+    /// table 7.5 
+    /// returns number of atoms per thermal fission of a nuclide
+    /// Lamarsh, J. R. (1975). Introduction to nuclear engineering.
+    #[inline]
+    pub fn fp_yield_iodine_135_from_u233_thermal_fission() -> Ratio {
+        Ratio::new::<ratio>(0.0475)
+    }
+    /// table 7.5 
+    /// returns number of atoms per thermal fission of a nuclide
+    /// Lamarsh, J. R. (1975). Introduction to nuclear engineering.
+    #[inline]
+    pub fn fp_yield_xe_135_from_u233_thermal_fission() -> Ratio {
+        Ratio::new::<ratio>(0.00107)
+    }
+
+
+    /// table 7.5 
+    /// Lamarsh, J. R. (1975). Introduction to nuclear engineering.
+    /// returns number of atoms per thermal fission of a nuclide
+    #[inline]
+    pub fn fp_yield_iodine_135_from_pu239_thermal_fission() -> Ratio {
+        Ratio::new::<ratio>(0.0604)
+    }
+    /// table 7.5 
+    /// Lamarsh, J. R. (1975). Introduction to nuclear engineering.
+    /// returns number of atoms per thermal fission of a nuclide
+    #[inline]
+    pub fn fp_yield_xe_135_from_pu239_thermal_fission() -> Ratio {
+        Ratio::new::<ratio>(0.00105)
+    }
 }
 
 impl Default for Xenon135Poisoning {
