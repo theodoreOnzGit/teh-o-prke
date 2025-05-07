6b535580f4f0af404895513d4c19ba050d9ce976
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-05-07 16:18:56 +0800

    added reference for rodas and rosenbrock solvers

---
+++ src/time_stepping/mod.rs
@@ -0,0 +1,13 @@
+// for Rodas solvers:
+//
+//     Hairer, E., Nørsett, S. P., & Wanner, G. (1996).
+//    Solving Ordinary Differential Equations II:
+//    Stiff and Differential-Algebraic Problems, second edition",
+//    Springer-Verlag, Berlin.
+//
+// for Rosenbrock solvers:
+//
+//     Verwer, J. G., Spee, E. J., Blom, J. G., & Hundsdorfer, W. (1999).
+//    A second-order Rosenbrock method applied to
+//    photochemical dispersion problems.
+//    SIAM Journal on Scientific Computing, 20(4), 1456-1480.
